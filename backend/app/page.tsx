0205505fcf221efd2b94994983f8b8ae3d2b794a
evanlin23 <evanlin366@gmail.com>
2025-10-01 11:36:34 -0500

    Update landing page to be a better example

--- backend/app/page.tsx
+++ backend/app/page.tsx
@@ -3,6 +3,10 @@
 import { trpc } from "@/lib/trpc";
 
 export default function Home() {
-  const helloQuery = trpc.hello.greet.useQuery();
-  return <div>{helloQuery?.data}</div>;
+  const { data, isLoading, error } = trpc.hello.greet.useQuery();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+
+  return <div>{data}</div>;
 }
