598609de5bc8013890c5af8b21c9113761ea07eb
Timothy Wei <timswei@gmail.com>
2025-10-03 19:48:50 -0500

    feat: created prisma video model and seed prisma schema (#8)
    * created prisma video model and seed prisma schema
    
    * fix linting errors
    
    * cleanup schema.prisma
    
    * fix enums
    
    * fix enums
    
    * added getAllVideos and addVideo endpoints
    
    * add unique video error handling
    
    ---------
    
    Co-authored-by: Timothy Wei <timothywei@Timothys-MacBook-Pro-1405.local>
    Co-authored-by: Timothy Wei <timothywei@Timothys-MacBook-Pro-1464.local>

--- backend/server/routers/index.ts
+++ backend/server/routers/index.ts
@@ -1,8 +1,10 @@
 import { router } from "../trpc";
 import { helloRouter } from "./hello";
+import { videosRouter } from "./videos";
 
 export const appRouter = router({
   hello: helloRouter,
+  videos: videosRouter,
 });
 
 export type AppRouter = typeof appRouter;
