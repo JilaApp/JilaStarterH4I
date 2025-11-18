23957e25fc0ffb3e0c859c502ce099838067e99d
ishaaty <ishatyagi888@gmail.com>
2025-11-18 15:21:38 -0600

    made skeleton for header

--- mobile-app/components/Header.tsx
+++ mobile-app/components/Header.tsx
@@ -1,5 +1,24 @@
+import { View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import React from "react";
 
+export default function GradientBox() {
+  return (
+    <LinearGradient
+      colors={["#E8965B", "#7E0601"]} // orange → red
+      start={{ x: 0, y: 0 }}          // top
+      end={{ x: 0, y: 1 }}            // bottom
+      style={{
+        height: 125,
+        justifyContent: "center",
+        alignItems: "center",
+        borderBottomLeftRadius: 30,
+        borderBottomRightRadius: 30,
+      }}
+    >
+      <View>
 
-export default function Header() {
-
+      </View>
+    </LinearGradient>
+  );
 }
