58ec5cb8ea3679ac917e915372e1f476e84b8aa1
evanlin23 <evanlin366@gmail.com>
2025-11-24 10:59:22 -0600

    Remove unneeded assets and comments

--- mobile-app/components/JilaText.tsx
+++ mobile-app/components/JilaText.tsx
@@ -1,19 +1,3 @@
-// import React, { ReactNode } from "react";
-// import { Text, TextProps } from "react-native";
-
-// export default function JilaText({
-//   children,
-//   ...props
-// }: {
-//   children: ReactNode;
-// } & TextProps) {
-//   return (
-//     <Text {...props} style={{ fontFamily: "Fustat" }}>
-//       {children}
-//     </Text>
-//   );
-// }
-
 import React, { ReactNode } from "react";
 import { Text, TextProps } from "react-native";
 
