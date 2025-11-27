165c4e79f0dda721c9400f2cf42057d1268252ec
unknown <tl87@illinois.edu>
2025-11-27 14:09:51 -0800

    added active state to nav bar

--- mobile-app/components/NavBar.tsx
+++ mobile-app/components/NavBar.tsx
@@ -31,22 +31,28 @@ export default function NavBar() {
       <Pressable
         hitSlop={{ top: 50, bottom: 50, left: 50, right: 50 }}
         onPress={handleJobPress}
+        style={styles.iconContainer}
       >
         <Briefcase color={colors.white[400]} size={sizes.icon.lg} />
+        {pathname === "/job" && <View style={styles.activeIndicator} />}
       </Pressable>
 
       <Pressable
         hitSlop={{ top: 50, bottom: 50, left: 50, right: 50 }}
         onPress={handleHomePress}
+        style={styles.iconContainer}
       >
         <House color={colors.white[400]} size={sizes.icon.lg} />
+        {pathname === "/" && <View style={styles.activeIndicator} />}
       </Pressable>
 
       <Pressable
         hitSlop={{ top: 50, bottom: 50, left: 50, right: 50 }}
         onPress={handleSocialPress}
+        style={styles.iconContainer}
       >
         <BookOpen color={colors.white[400]} size={sizes.icon.lg} />
+        {pathname === "/social" && <View style={styles.activeIndicator} />}
       </Pressable>
     </View>
   );
@@ -60,5 +66,16 @@ const styles = StyleSheet.create({
     paddingVertical: sizes.spacing.lg,
     alignItems: "center",
     backgroundColor: colors.jila[400],
+  },
+  iconContainer: {
+    alignItems: "center",
+    gap: 4,
+  },
+  activeIndicator: {
+    width: sizes.icon.lg - 4,
+    height: 4,
+    backgroundColor: colors.white[400],
+    borderRadius: 5,
+    marginTop: 4,
   },
 });
