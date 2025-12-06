9f0400b928793c1e393f724e366aac791cddd387
evanlin23 <evanlin366@gmail.com>
2025-12-05 20:01:06 -0600

    Fix button padding

--- admin-dashboard/components/shared/AddButton.tsx
+++ admin-dashboard/components/shared/AddButton.tsx
@@ -14,7 +14,7 @@ export default function AddButton({
   return (
     <button
       onClick={onClick}
-      className={`flex items-center gap-[10px] bg-jila-400 text-white px-[10px] py-[10px] h-[46px] rounded-[10px] hover:bg-type-400 cursor-pointer font-bold text-lg transition-colors ${className}`}
+      className={`flex items-center gap-[10px] bg-jila-400 text-white px-[24px] py-[24px] h-[46px] rounded-[10px] hover:bg-type-400 cursor-pointer font-bold text-lg transition-colors ${className}`}
     >
       <Plus size={24} />
       {label}
